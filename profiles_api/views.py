401f1f7516d8811bffd2e200c6068d7f7cd45990
Dylan Oelkers <dylan.oelkers@parivedasolutions.com>
2019-07-25 13:38:25 -0400

    Added testing view set, list functionality

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+from rest_framework import viewsets
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
+    
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
@@ -13,3 +18,40 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello ' + name
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Handle deletion of an object"""
+        return Response({'method': 'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using routers'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
